cb80bc3ede87e6f7e8e03ef64ff9952ef52caf61
Rohit Pal <rohit03993@gmail.com>
2025-11-19 13:23:38 +0530

    Fix login redirect to preserve port 8080

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(url(route('dashboard')));
+        return redirect(config('app.url') . '/dashboard');
     }
 
     /**
